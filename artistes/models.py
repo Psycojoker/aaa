d935705f9d82c82ed4093c021951ee8b4b5b95e7
Laurent Peuch <cortex@worlddomination.be>
2012-02-19 15:48:18 +0100

    init

---
+++ artistes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+    pic = models.FileField(upload_to="pics")
+    quote = models.CharField(max_length=500)
+    url = models.CharField(max_length=500)
---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from django.views.generic import ListView
+from artistes.models import Artiste
+
+urlpatterns = patterns('',
+    url(r'^$', ListView.as_view(model=Artiste), name='home'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
