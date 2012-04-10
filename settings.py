a897416d0c55c08f0508433b069de05b96a70357
Laurent Peuch <cortex@worlddomination.be>
2012-04-10 17:34:50 +0200

    [mod] tabs -> spaces

--- settings.py
+++ settings.py
@@ -15,12 +15,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-	'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-	'NAME': 'aaa.db',		       # Or path to database file if using sqlite3.
-	'USER': '',			 # Not used with sqlite3.
-	'PASSWORD': '',			 # Not used with sqlite3.
-	'HOST': '',			 # Set to empty string for localhost. Not used with sqlite3.
-	'PORT': '',			 # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'aaa.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -139,16 +139,16 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-	'mail_admins': {
-	    'level': 'ERROR',
-	    'class': 'django.utils.log.AdminEmailHandler'
-	}
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
     },
     'loggers': {
-	'django.request': {
-	    'handlers': ['mail_admins'],
-	    'level': 'ERROR',
-	    'propagate': True,
-	},
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
     }
 }
