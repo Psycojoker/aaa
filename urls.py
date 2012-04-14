b6b5b36b6280c52f34dd77a972d4d9e63baa43b7
Laurent Peuch <cortex@worlddomination.be>
2012-04-14 23:58:36 +0200

    [mod] only display published artistes on hom

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView
 from artistes.models import Artiste
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Artiste), name='home'),
+    url(r'^$', ListView.as_view(queryset=Artiste.objects.filter(published=True)), name='home'),
     url(r'^news/', include('basic.blog.urls')),
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
